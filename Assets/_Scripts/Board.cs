c29558ea746844894213da4b22c2c3e34dd02dbb
Dilara <dilaraismn@gmail.com>
2023-02-06 12:05:11 +0300

    Movement Anim & Key Spam Bug Fix

--- Assets/_Scripts/Board.cs
+++ Assets/_Scripts/Board.cs
@@ -7,8 +7,9 @@ public class Board : MonoBehaviour
 {
     [SerializeField] private Tile tilePrefab;
     [SerializeField] private TileState[] tileStates;
-    private Grid grid;
     private List<Tile> tiles;
+    private Grid grid;
+    private bool waiting;
 
     private void Awake()
     {
@@ -24,21 +25,24 @@ public class Board : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (!waiting)
         {
-            MoveTileCheck(Vector2Int.up, 0,1, 1, 1);
-        }
-        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            MoveTileCheck(Vector2Int.down, 0, 1, grid.height - 2, -1);
-        }
-        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            MoveTileCheck(Vector2Int.left, 1,1,0, 1);
-        }
-        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            MoveTileCheck(Vector2Int.right, grid.width - 2, -1, 0,1);
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                MoveTileCheck(Vector2Int.up, 0,1, 1, 1);
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                MoveTileCheck(Vector2Int.down, 0, 1, grid.height - 2, -1);
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                MoveTileCheck(Vector2Int.left, 1,1,0, 1);
+            }
+            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                MoveTileCheck(Vector2Int.right, grid.width - 2, -1, 0,1);
+            }
         }
     }
 
@@ -53,6 +57,8 @@ public class Board : MonoBehaviour
 
     private void MoveTileCheck(Vector2Int direction, int startX, int incrementX, int startY, int incrementY)
     {
+        bool changed = false;
+        
         for (int x = startX; x >= 0 && x < grid.width; x += incrementX)
         {
             for (int y = startY; y >= 0 && y < grid.height; y += incrementY)
@@ -60,13 +66,18 @@ public class Board : MonoBehaviour
                 Cell cell = grid.GetCell(x, y);
                 if (cell.occupied)
                 {
-                    MoveTile(cell.tile, direction);
+                    changed |= MoveTile(cell.tile, direction);
                 }
             }
         }
+
+        if (changed)
+        {
+            StartCoroutine(WaitForChanges());
+        }
     }
 
-    private void MoveTile(Tile tile, Vector2Int direction)
+    private bool MoveTile(Tile tile, Vector2Int direction)
     {
         Cell newCell = null;
         Cell adjacent = grid.GetAdjacentCell(tile.cell, direction);
@@ -85,6 +96,19 @@ public class Board : MonoBehaviour
         if (newCell != null)
         {
             tile.MoveToCell(newCell);
+            return true;
         }
+
+        return false;
+    }
+
+    private IEnumerator WaitForChanges()
+    {
+        waiting = true;
+        yield return new WaitForSeconds(.1f);
+        waiting = false;
+        
+        //create new tile
+        //check for game over
     }
 }
